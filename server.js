c4601b6d53828eb6f13cf35c139b4fed345c1b2c
Rogerio Chaves <rogeriocfj@gmail.com>
2017-03-25 18:20:40 -0300

    Change name of the root div

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ server.get('/', (req, res) => {
   const htmlPath = path.resolve(__dirname, 'build', 'index.html');
 
   fs.readFile(htmlPath, 'utf8', (err, html) => {
-    const rootElem = '<div id="root">';
+    const rootElem = '<div id="header-root">';
     const renderedApp = renderToString(React.createElement(App, null));
 
     res.send(html.replace(rootElem, rootElem + renderedApp));
