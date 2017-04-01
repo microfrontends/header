f8a1fcb1f437ed5b0cfda8d8923b3a8428513439
Rogerio Chaves <rogeriocfj@gmail.com>
2017-04-01 00:33:40 -0300

    Use webcomponents to render

--- src/index.js
+++ src/index.js
@@ -3,7 +3,9 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('header-root')
-);
+class Header extends HTMLElement {
+  attachedCallback() {
+    ReactDOM.render(<App />, this.createShadowRoot());
+  }
+}
+document.registerElement('microfrontends-header', Header);
